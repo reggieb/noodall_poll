6adbb8607197ff555e7faec74e8851b665695ab0
Rob Nichols <git@nicholshayes.co.uk>
2011-10-07 09:47:00 +0100

    Basic noodall_poll/show functionality working with test

--- app/controllers/noodall_poll/polls_controller.rb
+++ app/controllers/noodall_poll/polls_controller.rb
@@ -1,7 +1,8 @@
 module NoodallPoll
   class PollsController < ApplicationController
     def show
-     # @poll = Poll.find(params[:id])
+      @poll = Poll.find(params[:id])
+      render :partial => 'show'
     end
     
   
--- test/functional/noodall_poll/polls_controller_test.rb
+++ test/functional/noodall_poll/polls_controller_test.rb
@@ -8,7 +8,7 @@ module NoodallPoll
     end
 
     def test_show
-      get :show, :id => @poll.id
+      noodall_poll_get :show, :id => @poll.id
       assert_response :success
       assert_equal(@poll, assigns(:poll), "Poll should be passed to template")
     end
